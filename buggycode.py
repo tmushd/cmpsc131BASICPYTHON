cca020856d0b585258902c61d0f84202403d88a7
tmushd <122757043+tmushd@users.noreply.github.com>
2023-03-28 22:24:47 -0400

    Update buggycode.py

--- buggycode.py
+++ buggycode.py
@@ -1,4 +1,3 @@
-import math as m
 #fix the code without running it and explain why the bugs are bugs.
 def calc_Area():
     m = int(input("Enter 1 for rectangle, 2 for square, 3 for circle, 4 for triangle"))
@@ -11,7 +10,7 @@ def calc_Area():
         area = s*s
     elif m==3:
         r = int(input("Enter the radius of the circle"))
-        area = m.pi*r*r
+        area = 3.14*r*r
     elif m==4:
         b = int(input("Enter the base length for the triangle"))
         h = int(input("Enter the height of the triangle"))
