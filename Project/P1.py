a844883f05aa085f6d73fde6a2e73f9773715977
Utkarsh Choudhary <ufc5009@psu.edu>
2023-04-25 19:54:11 -0400

    Update P1.py

--- Project/P1.py
+++ Project/P1.py
@@ -1,39 +1,78 @@
-box = "./box-1.txt"#Enter your path here
-def cleanse(box):
+'''
+Name- mayank futnani arupula
+ID-mxa6067@psu.edu
+
+
+
+
+'''
+
+#m = "/Users/mayankarupula/Downloads/box-1.txt"
+m = "/home/box-1.txt"
+def cleanse(m):
+    L2=[]
+    L3=[]
+    s = ""
+    contents=open(m,'r')
+    #same as L4 solution.
+    l=contents.readline()
+    #l is a string form of contents
+    while l!='':
+        # use int() to convert to intergers
+        l = l.strip().split()
+        flag = 0
+        for i in range(len(L2)):
+            if L2[i][0] == l[0]:
+                flag = 1
+        if flag == 0:
+            L2+=[l]
+        l=contents.readline()
+
+    for i in range(len(L2)):
+        
+        for j in range(1, len(L2[i])):
+            L2[i][j] = int(L2[i][j])
+        
+    return L2
+#end of first function'''
+
     
-    clientList=[]#deliverable list for client.
-    strcmp = ""#string to compare with list
-    contents=open(box,'r')
-    lst=contents.readline()
-    while lst!='':
-        row=[]#add to clientlist row by row
-        if lst[0] not in strcmp[0:len(strcmp)]:
-            strcmp += lst
-            for val in lst.strip().split():
-                if val.isdigit():#check for integers
-                    row += int(val)
-                else:
-                    row += str(val)
-            clientList+=[row]
-        lst=contents.readline()#reads next line where control ended.
-    return clientList
+def color_count(k):
+    r = []
+    b = k
+    n = len(b)
     
-def color_count(li):
-    ret = []
-    a = li
-    n = len(a)
-    for i in range(n):
-        for j in range(n-i-1):
-            if len(a[j]) < len(a[j+1]):
-                a[j], a[j+1] = a[j+1], a[j]
-                #bubble sorting just in case the sorted list is required later.
-    maximum_balls = a[0][0]
-    ret += maximum_balls
-    minimum_balls = a[n-1][0]
-    ret += minimum_balls
-    return ret
+    max_num=len(b[0])
+    max_color=b[0][0]
+    for i in range(len(b)):
+        if (len(b[i])>max_num):
+            max_num=len(b[i])
+            max_color=b[i][0]
+
+
+    min_num=len(b[0])
+    min_color=b[0][0]
+    for i in range(len(b)):
+        if (len(b[i])<min_num):
+            min_num=len(b[i])
+            min_color=b[i][0]
+    return [max_color,min_color]
+#end of second function
+
+def color_deviation(cleanse(m)):
+    li = cleanse(m)
+    avg = []
+    S = 0.0#S = population standard deviation
+    for i in range(1,len(li[i])):
+        s+=li[i]
+    avg = s/
+        
+    
+    return S
     
 def main():
-    print(cleanse(box))
-    print(color_count(cleanse(box)))
+    print(cleanse(m))
+    print(color_count(a))
+
+#main for testing
 main()
