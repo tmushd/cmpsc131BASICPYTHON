7b8cd1d4cd9a4791f235adadfdcc51a9a4a5925d
Utkarsh Choudhary <ufc5009@psu.edu>
2023-05-05 23:40:54 -0400

    Update PX.py

--- Project/PX.py
+++ Project/PX.py
@@ -1,4 +1,4 @@
-import math
+import math as m
 '''
 Name- mayank futnani arupula
 ID-mxa6067@psu.edu
@@ -8,17 +8,17 @@ ID-mxa6067@psu.edu
 
 '''
 
-m = "/Users/mayankarupula/Downloads/box-1.txt"
-def cleanse(m):
+c = "/Users/mayankarupula/Downloads/box-1.txt"
+def cleanse(c):
     L2=[]
     L3=[]
     s = ""
-    contents=open(m,'r')
+    contents=open(c,'r')
     #same as L4 solution.
     l=contents.readline()
     #l is a string form of contents
     while l!='':
-        # use int() to convert to intergers
+        # use int() to convert to integers
         l = l.strip().split()
         flag = 0
         for i in range(len(L2)):
@@ -34,73 +34,73 @@ def cleanse(m):
             L2[i][j] = int(L2[i][j])
         
     return L2
-#end of first function'''
+#end of first function
 
     
 def color_count(k):
     r = []
     b = k
     n = len(b)
-    
     max_num=len(b[0])
     max_color=b[0][0]
     for i in range(len(b)):
         if (len(b[i])>max_num):
             max_num=len(b[i])
             max_color=b[i][0]
-
-
+            #max color stored
     min_num=len(b[0])
     min_color=b[0][0]
     for i in range(len(b)):
         if (len(b[i])<min_num):
             min_num=len(b[i])
             min_color=b[i][0]
+            #min color stored
     return [max_color,min_color]
+# returned as list
 #end of second function
 
-def color_deviation(cleanse(m)):
-    li = cleanse(m)
-    s1 = 0.0#sum to calculate mean
-    s2 = 0.0#sum to calculate (xi - mean)^2
-    avg = 0.0#stores mean
-    a = []#client list
-    S = 0.0#S = population standard deviation
-    for i in range(len(li)):
-        for j in range(1, len(li[i]))
-            s1+=li[i][j]
-        avg = s1/len(li[i])
-        for k in range(1, len(li[i]))
-            s2 += (li[i][j]-avg)**2
-        S = math.sqrt(s2/len(li[i]))
+def color_deviation(cleanse(c)):
+    l1 = cleanse(c)
+    s1 = 0.0
+    s2 = 0.0
+    mean = 0.0
+    a = []
+    S = 0.0
+    for i in range(len(l1)):
+        for j in range(1, len(l1[i]))
+            s1+=l1[i][j]
+        mean = s1/len(l1[i])
+        for k in range(1, len(l1[i]))
+            s2 += (m.pow((l1[i][j]-mean),2))
+        S = m.sqrt(s2/len(l1[i]))
         a+=S
     return a
 #end of third function
 
-def rainbow_check(cleanse(m)):
-    li = cleanse(m)
+def rainbow_check(cleanse(c)):
+    l1 = cleanse(c)
     r = -6
     a=[]
-    for i in range(len(li))
-        if li[i] == 'Violet':
+    for i in range(len(l1))#VIBGYOR
+        if l1[i] == 'Violet':
             r+=1
             a+=i
-        if li[i] == 'Indigo':
+        if l1[i] == 'Indigo':
             r+=1
             a+=i
-        if li[i] == 'Blue':
+        if l1[i] == 'Blue':
             r+=1
             a+=i
-        if li[i] == 'Green':
+        if l1[i] == 'Green':
             r+=1
             a+=i
-        if li[i] == 'Yellow':
+        if l1[i] == 'Yellow':
             r+=1
             a+=i
-        if li[i] == 'Orange':
+        if l1[i] == 'Orange':
             r+=1
             a+=i
-        if li[i] == 'Red':
+        if l1[i] == 'Red':
             r+=1
             a+=i
     if r==1:
@@ -109,18 +109,19 @@ def rainbow_check(cleanse(m)):
         return a
 #end of fourth function        
 
-def happy_kids(cleanse(m)):
+def happy_kids(cleanse(c)):
     x = input("Enter the value of x: ")
-    li = cleanse(m)
-    c = 0
-    for i in range(len(li)):
-        if x == li[i]:
-            c+=1
-    return c
+    l1 = cleanse(c)
+    a = 0
+    for i in range(len(l1)):
+        if x == l1[i]:
+            a+=1
+    return a
 #end of fifth function
     
     
 def main():
+    #main for checks
     print(cleanse(m))
     a = cleanse(m)
     print(color_count(a))
